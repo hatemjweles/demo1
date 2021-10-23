0b70185f81a12fe6d3a628bddba0351ecd9de26c
hatemjweles <32652946+hatemjweles@users.noreply.github.com>
2021-10-23 16:36:43 +0300

    done

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import math
 import os
+import math
 
 
-def add(a, b) -> str:
+def add(a, b) -> int:
     return math.floor(a + b)
 
 
-def to_sentence(s) -> int:
+def to_sentence(s) -> str:
     s = s.capitalize()
 
     if s.endswith('.'):
