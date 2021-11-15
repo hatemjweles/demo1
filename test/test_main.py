0684b42bfdc8d373483f8b2a03e86808c535fe0f
hatemjweles <32652946+hatemjweles@users.noreply.github.com>
2021-11-15 16:16:05 +0200

    hi

--- test/test_main.py
+++ test/test_main.py
@@ -3,7 +3,7 @@ import main
 
 def test_add():
     assert main.add(3, 4) == 7
-    assert main.add(3.5, 4) == 8
+    assert main.add(3.5, 4) == 10
     assert main.add(3.9, 4) == 7
     assert main.add(3.9, 4.1) == 8
 
